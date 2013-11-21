4e1d4a8402e0bc46f9155e7b1ab40f66de66ef95
Asuka Shinohara <dryerrr@gmail.com>
2013-11-21 16:00:55 +0900

    131121

--- calc/ViewController.h
+++ calc/ViewController.h
@@ -8,6 +8,24 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+    int currentNumber;
+}
+
+- (IBAction)inputNumber0:(id)sender;
+- (IBAction)inputNumber1:(id)sender;
+- (IBAction)inputNumber2:(id)sender;
+- (IBAction)inputNumber3:(id)sender;
+- (IBAction)inputNumber4:(id)sender;
+- (IBAction)inputNumber5:(id)sender;
+- (IBAction)inputNumber6:(id)sender;
+- (IBAction)inputNumber7:(id)sender;
+- (IBAction)inputNumber8:(id)sender;
+- (IBAction)inputNumber9:(id)sender;
+
+
+
+
+@property (weak, nonatomic) IBOutlet UILabel *numberOutput;
 
 @end
--- calc/ViewController.m
+++ calc/ViewController.m
@@ -26,4 +26,53 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)inputNumber0:(id)sender {
+    currentNumber = currentNumber * 10;
+    _numberOutput.text = [NSString stringWithFormat:@"%d",currentNumber];
+}
+- (IBAction)inputNumber1:(id)sender {
+    currentNumber = currentNumber * 10;
+    currentNumber = currentNumber + 1;
+    _numberOutput.text = [NSString stringWithFormat:@"%d",currentNumber];
+}
+- (IBAction)inputNumber2:(id)sender {
+    currentNumber = currentNumber * 10;
+    currentNumber = currentNumber + 2;
+    _numberOutput.text = [NSString stringWithFormat:@"%d",currentNumber];
+}
+- (IBAction)inputNumber3:(id)sender {
+    currentNumber = currentNumber * 10;
+    currentNumber = currentNumber + 3;
+    _numberOutput.text = [NSString stringWithFormat:@"%d",currentNumber];
+}
+- (IBAction)inputNumber4:(id)sender {
+    currentNumber = currentNumber * 10;
+    currentNumber = currentNumber + 4;
+    _numberOutput.text = [NSString stringWithFormat:@"%d",currentNumber];
+}
+- (IBAction)inputNumber5:(id)sender {
+    currentNumber = currentNumber * 10;
+    currentNumber = currentNumber + 5;
+    _numberOutput.text = [NSString stringWithFormat:@"%d",currentNumber];
+}
+- (IBAction)inputNumber6:(id)sender {
+    currentNumber = currentNumber * 10;
+    currentNumber = currentNumber + 6;
+    _numberOutput.text = [NSString stringWithFormat:@"%d",currentNumber];
+}
+- (IBAction)inputNumber7:(id)sender {
+    currentNumber = currentNumber * 10;
+    currentNumber = currentNumber + 7;
+    _numberOutput.text = [NSString stringWithFormat:@"%d",currentNumber];
+}
+- (IBAction)inputNumber8:(id)sender {
+    currentNumber = currentNumber * 10;
+    currentNumber = currentNumber + 8;
+    _numberOutput.text = [NSString stringWithFormat:@"%d",currentNumber];
+}
+- (IBAction)inputNumber9:(id)sender {
+    currentNumber = currentNumber * 10;
+    currentNumber = currentNumber + 9;
+    _numberOutput.text = [NSString stringWithFormat:@"%d",currentNumber];
+}
 @end
